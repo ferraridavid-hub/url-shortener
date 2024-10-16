aee040e905b783aa5dec66639f59e3e1c276e3b7
davidferrari <ferraridavid.hub@gmail.com>
2024-10-16 14:06:06 +0200

    First commit

---
+++ src/test/java/com/coding/challenges/url_shortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.challenges.url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
